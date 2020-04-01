1fc6731b15216a4e5832c1646692fadd0c8d86e2
hanyaxiong <hanyaxiong@icfo.cn>
2020-04-01 11:53:08 +0800

    fix bug

--- src/com/concurrent/T01_ConcurrentMap.java
+++ src/com/concurrent/T01_ConcurrentMap.java
@@ -9,9 +9,9 @@ public class T01_ConcurrentMap {
     public static void main(String[] args) throws InterruptedException {
         Map<String, String> map = new ConcurrentHashMap<>();         // 默认把容器分成16段，插入时只锁定其中一段
         // Map<String, String> map = new ConcurrentSkipListMap<>();     // 排好序的，插入效率高
-        // Map<String, String> map = new Hashtable<String, String>();   // 添加时会锁定整个对象
-        // Map<String, String> map = new HashMap<String, String>();
-        //Collections.synchronizedMap()
+        // Map<String, String> map = new Hashtable<>();   // 添加时会锁定整个对象
+        // Map<String, String> smap = new HashMap<>();
+        // Map<String, String> map = Collections.synchronizedMap(smap);
         Random random = new Random();
 
         Thread[] threads = new Thread[500];
